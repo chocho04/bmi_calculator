9ba3123f012bae37c7944b25f647096427cb26ca
chocho04 <chochosan044@gmail.com>
2022-07-02 16:38:45 +0300

    3

---
+++ lib/constants.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kPinkTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontWeight: FontWeight.w400,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xff24d876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+const kBottomContainerHeight = 80.0;
+const kBottomContainerColor = Color(0xFFEB1555);
+const kInactiveCardColor = Color(0xFF111328);
+const kActiveCardColor = Color(0xFF1D1E33);
